42d76b1501d4ff13e87549f8fd16e219a8e7116c
Bilal Aksoy <sloeyeleet@gmail.com>
2024-06-23 17:25:05 +0300

    Created songs page UI with actual data

--- lib/features/home/view/pages/songs_page.dart
+++ lib/features/home/view/pages/songs_page.dart
@@ -1,13 +1,90 @@
+import 'package:client/core/theme/app_colors.dart';
+import 'package:client/core/widgets/loader.dart';
+import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SongsPage extends StatelessWidget {
+class SongsPage extends ConsumerWidget {
   const SongsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Songs Page'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Latest today',
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
+            ),
+          ),
+          ref.watch(getAllSongsProvider).when(
+                data: (songs) {
+                  return SizedBox(
+                    height: 260,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: songs.length,
+                      itemBuilder: (context, index) {
+                        final song = songs[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 180,
+                                height: 180,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7),
+                                  image: DecorationImage(
+                                    image: NetworkImage(song.thumbnail_url),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              SizedBox(
+                                width: 180,
+                                child: Text(
+                                  song.song_name,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  maxLines: 1,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 180,
+                                child: Text(
+                                  song.artist,
+                                  style: const TextStyle(
+                                    color: AppColors.subtitleText,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w500,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  maxLines: 1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+                error: (error, st) {
+                  return Center(child: Text('Error: $error'));
+                },
+                loading: () => const Loader(),
+              ),
+        ],
       ),
     );
   }
